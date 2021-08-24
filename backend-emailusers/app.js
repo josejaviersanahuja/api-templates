0f57a51fbcc5a9a4f9c69eb8a87e3abb732cb31f
josejavier <82359964+josejaviersanahuja@users.noreply.github.com>
2021-08-24 16:56:14 +0100

    api template protected against brute force attacks

--- backend-emailusers/app.js
+++ backend-emailusers/app.js
@@ -2,15 +2,45 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+
+//Securing headers
 const helmet = require('helmet')
 
+// against brute force attacks Dependencies
+const rateLimit = require("express-rate-limit");
+const slowDown = require("express-slow-down");
+
+// against brute force attacks Configuration
+const limiter = rateLimit({
+  windowMs:  60 * 1000, // in 1 minute
+  max: 60 // limit each IP to 60 requests per minute
+});
+const speedLimiter = slowDown({
+  windowMs: 60 * 1000, // in 1 minutes
+  delayAfter: 20, // allow 20 requests per minute, then...
+  delayMs: 500 // begin adding 500ms of delay per request above 20:
+  // request # 21 is delayed by  500ms
+  // request # 22 is delayed by 1000ms
+  // request # 233 is delayed by 1500ms
+  // etc.
+});
+
+// router endpoints
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var tokensRouter = require('./routes/tokens');
 var validationRouter = require('./routes/validation');
 
+//Initializing APP
 var app = express();
+
+//securing headers
 app.use(helmet())
+
+//securing against brute force attacks
+app.use(speedLimiter);
+app.use(limiter);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
